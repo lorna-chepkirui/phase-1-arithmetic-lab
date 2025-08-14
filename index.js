53cf6ea707d20083d98999b998a79f63d6a0392b
lorna chepkirui <lorna@tililtech.com>
2025-08-14 00:11:58 -0700

    Done2

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 
 // Write your code here
-const num1 = 31;
-const num2 = 2;
-const multiply = num1 * num2;
+// const num1 = 31;
+// const num2 = 2;
+// const multiply = num1 * num2;
 
-// Write your code here 
-const random = Math.floor( Math.random() * 10 + 1 );
+// // Write your code here 
+// const random = Math.floor( Math.random() * 10 + 1 );
 
-const num3 = 16;
-const num4 = 6;
-const mod = num3 % num4;
+// const num3 = 16;
+// const num4 = 6;
+// const mod = num3 % num4;
 
-const max = Math.max(20, 15, 10, 5, 1) 
+// const max = Math.max(20, 15, 10, 5, 1) 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(20, 15, 10, 5, 1);
